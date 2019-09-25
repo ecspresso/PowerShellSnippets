f28aeb5d8c01993ca1966cc51c908be5af596a78
Emile <emile@upstream.se>
2019-09-25 10:18:42 +0200

    Add files via upload

---
+++ move every X files to folder X.ps1
@@ -0,0 +1,14 @@
+﻿$fileCount = 0
+$incrementer = 1000
+$folderNumber = 1000
+$ls = ls
+$folder = New-Item -Path .\ -Name $folderNumber -ItemType "directory"
+foreach($file in $ls) {
+    Move-Item -Path $file.FullName -Destination $folder.FullName
+
+    if($fileCount -eq $folderNumber) {
+        $folderNumber += $incrementer
+        $folder = New-Item -Path .\ -Name $folderNumber -ItemType "directory"
+    }
+    $fileCount++
+}
